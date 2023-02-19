765d081b74cac34a7cf3d59ab4c2d3c0da4ced1f
hoangtu <hoangtu@ohlaalaa.com>
2023-02-19 16:23:26 +0700

    Init Kols

---
+++ storage/framework/views/143d076c64c13047b17477624f435256d3e45d5e.php
@@ -0,0 +1,118 @@
+
+<?php $__env->startSection('title'); ?> Job Grid Lists <?php $__env->stopSection(); ?>
+<?php $__env->startSection('content'); ?>
+<?php $__env->startComponent('components.breadcrumb'); ?>
+<?php $__env->slot('li_1'); ?> Jobs <?php $__env->endSlot(); ?>
+<?php $__env->slot('title'); ?> Job Grid Lists <?php $__env->endSlot(); ?>
+<?php echo $__env->renderComponent(); ?>
+
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card">
+            <div class="card-body">
+                <form>
+                    <div class="row g-3">
+                        <div class="col-xxl-4 col-sm-12">
+                            <div class="search-box">
+                                <input type="text" class="form-control search bg-light border-light" id="searchJob" autocomplete="off" placeholder="Search for jobs or companies...">
+                                <i class="ri-search-line search-icon"></i>
+                            </div>
+                        </div>
+                        <!--end col-->
+                        <div class="col-xxl-3 col-sm-4">
+                            <input type="text" class="form-control bg-light border-light" id="datepicker" data-provider="flatpickr" data-date-format="d M, Y" data-range-date="true" placeholder="Select date">
+                        </div>
+                        <!--end col-->
+                        <div class="col-xxl-2 col-sm-4">
+                            <div class="input-light">
+                                <select class="form-control" data-choices data-choices-search-false name="choices-idType" id="idType">
+                                    <option value="all" selected>All</option>
+                                    <option value="Full Time">Full Time</option>
+                                    <option value="Part Time">Part Time</option>
+                                    <option value="Intership">Intership</option>
+                                    <option value="Freelance">Freelance</option>
+                                </select>
+                            </div>
+                        </div>
+                        <!--end col-->
+
+                        <div class="col-xxl-2 col-sm-4">
+                            <div class="input-light">
+                                <select class="form-control" data-choices data-choices-search-false name="choices-single-default" id="idStatus">
+                                    <option value="all" selected>All</option>
+                                    <option value="Active">Active</option>
+                                    <option value="New">New</option>
+                                    <option value="Close">Close</option>
+                                </select>
+                            </div>
+                        </div>
+                        <!--end col-->
+
+                        <div class="col-xxl-1 col-sm-4">
+                            <button type="button" class="btn btn-primary w-100" onclick="filterData();">
+                                <i class="ri-equalizer-fill me-1 align-bottom"></i> Filters
+                            </button>
+                        </div>
+                        <!--end col-->
+                    </div>
+                    <!--end row-->
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end row -->
+
+<div class="row">
+    <div class="col-lg-12">
+        <div class="d-flex align-items-center mb-4">
+            <div class="flex-grow-1">
+                <p class="text-muted fs-14 mb-0">Result: <span id="total-result"></span></p>
+            </div>
+            <div class="flex-shrink-0">
+                <div class="dropdown">
+                    <a class="text-muted fs-14 dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                        All View
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end row -->
+
+<div class="row" id="job-list">
+</div>
+<!-- end row -->
+
+<div class="row g-0 justify-content-end mb-4" id="pagination-element">
+    <!-- end col -->
+    <div class="col-sm-6">
+        <div class="pagination-block pagination pagination-separated justify-content-center justify-content-sm-end mb-sm-0">
+            <div class="page-item">
+                <a href="javascript:void(0);" class="page-link" id="page-prev">Previous</a>
+            </div>
+            <span id="page-num" class="pagination"></span>
+            <div class="page-item">
+                <a href="javascript:void(0);" class="page-link" id="page-next">Next</a>
+            </div>
+        </div>
+    </div><!-- end col -->
+</div>
+<!-- end row -->
+
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('script'); ?>
+<!-- job-candidate-grid js -->
+<script src="<?php echo e(URL::asset('assets/js/pages/job-grid-list.init.js')); ?>"></script>
+
+<!-- App js -->
+<script src="<?php echo e(URL::asset('assets/js/app.min.js')); ?>"></script>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Tarun\Laravel Admins\velzon_laravel\velzon_laravel_v2.0\default\Admin\resources\views/apps-job-grid-lists.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/214813357279d64b314ecf3dc9da29e20d2d6166.php
@@ -0,0 +1,173 @@
+<?php $__env->startSection('title'); ?> <?php echo app('translator')->get('translation.Apex_Area_Chart'); ?> <?php $__env->stopSection(); ?>
+<?php $__env->startSection('content'); ?>
+
+<?php $__env->startComponent('components.breadcrumb'); ?>
+<?php $__env->slot('li_1'); ?> Apexcharts <?php $__env->endSlot(); ?>
+<?php $__env->slot('title'); ?> Apex Area Charts <?php $__env->endSlot(); ?>
+<?php echo $__env->renderComponent(); ?>
+<div class="row">
+    <div class="col-xl-6">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title mb-0">Basic Area Chart</h4>
+            </div><!-- end card header -->
+
+            <div class="card-body">
+                <div id="area_chart_basic" data-colors='["--vz-success"]' class="apex-charts" dir="ltr"></div>
+            </div><!-- end card-body -->
+        </div><!-- end card -->
+    </div>
+    <!-- end col -->
+
+    <div class="col-xl-6">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title mb-0">Spline Area Chart</h4>
+            </div><!-- end card header -->
+
+            <div class="card-body">
+                <div id="area_chart_spline" data-colors='["--vz-primary", "--vz-success"]' class="apex-charts" dir="ltr"></div>
+            </div><!-- end card-body -->
+        </div><!-- end card -->
+    </div>
+    <!-- end col -->
+</div>
+<!-- end row -->
+
+<div class="row">
+    <div class="col-xl-6">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title mb-0">Area Chart - Datetime X - Axis Chart</h4>
+            </div><!-- end card header -->
+
+            <div class="card-body">
+                <div>
+                    <div class="toolbar d-flex align-items-start justify-content-center flex-wrap gap-2">
+                        <button type="button" class="btn btn-soft-primary timeline-btn btn-sm" id="one_month">
+                            1M
+                        </button>
+                        <button type="button" class="btn btn-soft-primary timeline-btn btn-sm" id="six_months">
+                            6M
+                        </button>
+                        <button type="button" class="btn btn-soft-primary timeline-btn btn-sm active" id="one_year">
+                            1Y
+                        </button>
+                        <button type="button" class="btn btn-soft-primary timeline-btn btn-sm" id="all">
+                            ALL
+                        </button>
+                    </div>
+                    <div id="area_chart_datetime" data-colors='["--vz-info"]' class="apex-charts" dir="ltr"></div>
+                </div>
+            </div><!-- end card-body -->
+        </div><!-- end card -->
+    </div>
+    <!-- end col -->
+
+    <div class="col-xl-6">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title mb-0">Area with Negative Values Chart</h4>
+            </div><!-- end card header -->
+
+            <div class="card-body">
+                <div id="area_chart_negative" data-colors='["--vz-success", "--vz-info"]' class="apex-charts" dir="ltr"></div>
+            </div><!-- end card-body -->
+        </div><!-- end card -->
+    </div>
+    <!-- end col -->
+</div>
+<!-- end row -->
+
+<div class="row">
+    <div class="col-xl-6">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title mb-0">Area Chart - Github Style</h4>
+            </div><!-- end card header -->
+
+            <div class="card-body">
+                <div>
+                    <div class="bg-light">
+                        <div id="area_chart-months" data-colors='["--vz-danger"]' class="apex-charts" dir="ltr"></div>
+                    </div>
+
+                    <div class="github-style d-flex align-items-center my-2">
+                        <div class="flex-shrink-0 me-2">
+                            <img class="avatar-sm rounded" src="<?php echo e(URL::asset('assets/images/users/avatar-2.jpg')); ?>" data-hovercard-user-id="634573" alt="" />
+                        </div>
+                        <div class="flex-grow-1">
+                            <a class="font-size-14 text-dark fw-medium">coder</a>
+                            <div class="cmeta text-muted font-size-11">
+                                <span class="commits text-dark fw-medium"></span> commits
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="bg-light">
+                        <div id="area_chart-years" data-colors='["--vz-success"]' class="apex-charts" dir="ltr"></div>
+                    </div>
+                </div>
+            </div><!-- end card-body -->
+        </div><!-- end card -->
+    </div>
+    <!-- end col -->
+
+    <div class="col-xl-6">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title mb-0">Stacked Area Chart</h4>
+            </div><!-- end card header -->
+
+            <div class="card-body">
+                <div id="area_chart_stacked" data-colors='["--vz-success", "--vz-info", "--vz-light"]' class="apex-charts" dir="ltr"></div>
+            </div><!-- end card-body -->
+        </div><!-- end card -->
+    </div>
+    <!-- end col -->
+</div>
+<!-- end row -->
+
+<div class="row">
+    <div class="col-xl-6">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title mb-0">Irregular Timeseries Chart</h4>
+            </div><!-- end card header -->
+
+            <div class="card-body">
+                <div id="area_chart_irregular" data-colors='["--vz-primary", "--vz-warning", "--vz-success"]' class="apex-charts" dir="ltr"></div>
+            </div><!-- end card-body -->
+        </div><!-- end card -->
+    </div>
+    <!-- end col -->
+
+    <div class="col-xl-6">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title mb-0">Area Chart With Null Values Chart</h4>
+            </div><!-- end card header -->
+
+            <div class="card-body">
+                <div id="area-missing-null-value" data-colors='["--vz-success"]' class="apex-charts" dir="ltr"></div>
+            </div><!-- end card-body -->
+        </div><!-- end card -->
+    </div>
+    <!-- end col -->
+</div>
+<!-- end row -->
+<?php $__env->stopSection(); ?>
+<?php $__env->startSection('script'); ?>
+<script src="<?php echo e(URL::asset('assets/libs/apexcharts/apexcharts.min.js')); ?>"></script>
+<!-- for basic area chart -->
+<script src="https://img.themesbrand.com/velzon/apexchart-js/stock-prices.js"></script>
+<!-- for github style chart -->
+<script src="https://img.themesbrand.com/velzon/apexchart-js/github-data.js"></script>
+<!-- for irregular timeseries chart -->
+<script src="https://img.themesbrand.com/velzon/apexchart-js/irregular-data-series.js"></script>
+<script src="<?php echo e(URL::asset('assets/libs/moment/moment.min.js')); ?>"></script>
+<script src="<?php echo e(URL::asset('assets/js/pages/apexcharts-area.init.js')); ?>"></script>
+<script src="<?php echo e(URL::asset('/assets/js/app.min.js')); ?>"></script>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Tarun\Laravel Admins\velzon_laravel\velzon_laravel_v2.0\default\Admin\resources\views/charts-apex-area.blade.php ENDPATH**/ ?>
